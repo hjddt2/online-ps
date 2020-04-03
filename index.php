3236c93c5becae89e073a04983e973fd53761a48
hjddt2 <hujiandon@163.com>
2020-04-03 09:16:10 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang='zh-cn'>
+<head>
+	<meta charset='utf-8'>
+	<meta name='viewport' content='width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0'>
+	<title>在线PS-电脑极客分享www.cfanpc.com</title>
+	<meta name='keywords' content='在线ps,ps网页版,ps在线图片处理'>
+    <meta name='description' content='设计PS是一款专业精简的在线ps图片处理软件，免下载、免安装，直接在浏览器打开网页版就可随时随地用它修正，调整和美化您的图片。'>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+	<link rel='stylesheet' href='style/a.css'>
+	<link rel='stylesheet' href='style/font.css'>
+	<style>
+		h1 {
+			font-size: 2.5em;
+			margin: 0.7em 0;
+			letter-spacing: 0em;
+		}
+
+		strong {
+			display: inline-block;
+			font-size: 1.1em;
+			background: rgba(0, 0, 0, 0.1);
+			padding: 0.1em 0.5em;
+			margin: 0.2em 0.5em 0.2em 0;
+			border-radius: 0.3em;
+			font-weight: normal;
+			line-height: 1.7
+		}
+	</style>
+	<script>
+		var googletag = googletag || {};
+		googletag.cmd = googletag.cmd || [];
+	</script>
+</head>
+
+<body class='theme0'>
+	<div id='cap' style='display:none;'></div>
+	<div class='topfloat' style='display:none;'></div>
+	<script src='code/a.js'></script>
+	<script src='code/b.js'></script>
+	<script src='code/c.js'></script>
+	<script src='code/d.js'></script>
+	<script src='code/e.js'></script>
+	<script src='code/f.js'></script>
+
+</body>
+
+</html>
